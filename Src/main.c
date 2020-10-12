43ec750df01b122413d842bb8f2d96e54b683da8
Tom Wallis <thomaswallis0@gmail.com>
2020-10-12 01:12:09 -0400

    Modify main

--- Src/main.c
+++ Src/main.c
@@ -1,18 +1,19 @@
 /* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
 #include "main.h"
 #include "adc.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
-#include <stdint.h>
-#include <stdio.h>
 #include "motor.h"
 #include "log.h"
 #include "ultrasonic.h"
+#include "ble.h"
 
 /* Private variables ---------------------------------------------------------*/
-uint32_t backLeftMotorEnc = 0;
-uint32_t backRightMotorEnc = 0;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
@@ -43,122 +44,19 @@ int main(void)
     log_Init();
     log_SetEnable(true);
     log_SetLevel(LOG_DEBUG);
-  
-    HAL_TIM_Base_Start(&htim2);
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4);
-  
-    HAL_TIM_Base_Start_IT(&htim14);
-    
-    MotorHandle_t backLeftMotor = 
-    {
-        .input1Timer = &htim3,
-        .input2Timer = &htim3,
-        .input1 = TIM_CHANNEL_1,
-        .input2 = TIM_CHANNEL_2,
-        .direction = MOTOR_NEUTRAL,
-        .dutyCycle = 0
-    };
-  
-    MotorHandle_t backRightMotor = 
-    {
-        .input1Timer = &htim3,
-        .input2Timer = &htim3,
-        .input1 = TIM_CHANNEL_3,
-        .input2 = TIM_CHANNEL_4,
-        .direction = MOTOR_NEUTRAL,
-        .dutyCycle = 0
-    };
-  
-    MotorHandle_t frontLeftMotor = 
-    {
-        .input1Timer = &htim4,
-        .input2Timer = &htim4,
-        .input1 = TIM_CHANNEL_2,
-        .input2 = TIM_CHANNEL_3,
-        .direction = MOTOR_NEUTRAL,
-        .dutyCycle = 0
-    };
-    
-    MotorHandle_t frontRightMotor = 
-    {
-        .input1Timer = &htim2,
-        .input2Timer = &htim2,
-        .input1 = TIM_CHANNEL_3,
-        .input2 = TIM_CHANNEL_4,
-        .direction = MOTOR_NEUTRAL,
-        .dutyCycle = 0
-    };
-    
-    motor_Init(REAR_LEFT, backLeftMotor);
-    motor_Init(REAR_RIGHT, backRightMotor);
-    motor_Init(FRONT_LEFT, frontLeftMotor);
-    motor_Init(FRONT_RIGHT, frontRightMotor);
-  
-    motor_SetDirection(REAR_LEFT, MOTOR_FORWARD);
-    motor_SetDirection(REAR_RIGHT, MOTOR_FORWARD);
-    motor_SetDirection(FRONT_LEFT, MOTOR_FORWARD);
-    motor_SetDirection(FRONT_RIGHT, MOTOR_FORWARD);
-  
-    HAL_ADC_Start(&hadc1);
-  
-    uint32_t msTicks = 0;
-    uint8_t dutyCycle = 0;
-  
-    GPIO_reference_t trigPin = 
-    {
-        .port = GPIOD,
-        .pin = GPIO_PIN_14,
-    };
-    GPIO_reference_t echoPin = 
-    {
-        .port = GPIOD,
-        .pin = GPIO_PIN_15,
-    };
 
-    GPIO_reference_t pingPin = 
-    {
-        .port = GPIOD,
-        .pin = GPIO_PIN_13,
-    };
-    
-    UltrasonicHandle_t hcsr04Handle = ultrasonic_Init(trigPin, echoPin, HC_SR04);
-    UltrasonicHandle_t pingHandle = ultrasonic_Init(pingPin, pingPin, PING);
+    ble_Setup(&huart2);
+    log_SetOutput(UART_TTL);
 
     /* Infinite loop */
+
+    static uint32_t msTicks = 0;
     while (1)
     {
-        if (HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK)
+        if (HAL_GetTick() - msTicks > 1000)
         {
-            dutyCycle = HAL_ADC_GetValue(&hadc1);
-            motor_SetSpeed(REAR_LEFT, dutyCycle);
-            motor_SetSpeed(REAR_RIGHT, dutyCycle);
-            motor_SetSpeed(FRONT_LEFT, dutyCycle);
-            motor_SetSpeed(FRONT_RIGHT, dutyCycle);
-        }
-        else
-        {
-            Error_Handler();
-        }
-  
-        if (HAL_GetTick() - msTicks > 500)
-        {
-            // print the encoder speed.
-            //log_DEBUG("%d, %lu", dutyCycle, backRightMotorEnc);
             msTicks = HAL_GetTick();
-            backLeftMotorEnc = 0;
-            backRightMotorEnc = 0;
-        }
-  
-        uint16_t distance = ultrasonic_Read(&hcsr04Handle, 1000);
-        uint16_t pingDistance = ultrasonic_Read(&pingHandle, 1000);
-        if (hcsr04Handle.state == COMPLETE && distance < 1000)
-        {
-            log_DEBUG("HCSR04 %d", distance);
-        }
-        if (pingHandle.state == COMPLETE && pingDistance < 1000)
-        {
-            log_DEBUG("Ping %d", pingDistance);
+            log_DEBUG("send");
         }
     }
 }
@@ -214,30 +112,7 @@ void SystemClock_Config(void)
  */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) 
 {
-    switch (GPIO_Pin) 
-    {
-        case GPIO_PIN_0: 
-        {
-            break;
-        }
 
-        case GPIO_PIN_4: 
-        {
-            if (HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_4) == GPIO_PIN_SET)
-            {
-                backRightMotorEnc++;
-            }
-            break;
-        }
-
-        case GPIO_PIN_5: 
-        {
-            backLeftMotorEnc++;
-            break;
-        }
-
-        default: { break; }
-    }
 }
 
 /**
